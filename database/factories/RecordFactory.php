a6adfa35b187cda05d0877a3a28538bffe69dcd8
memochou1993 <memochou1993@gmail.com>
2019-03-27 20:15:22 +0800

    Remove auth

--- database/factories/RecordFactory.php
+++ database/factories/RecordFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Record::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence(),
-        'content' => $faker->paragraph(),
-        'private' => $faker->boolean(),
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'private' => $faker->boolean,
         'user_id' => $faker->numberBetween(1, config('seeds.user.number')),
         'type_id' => $faker->numberBetween(1, count(config('factories.type'))),
         'created_at'  => now(),
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Tag::class, function (Faker $faker) {
     return [
-        'name' => $faker->word(),
+        'name' => $faker->word,
         'user_id' => $faker->numberBetween(1, config('seeds.user.number')),
     ];
 });
